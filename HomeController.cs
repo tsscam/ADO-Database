6d4a5dddc3f893902b3e441c54ef1603059872c3
Theresa Skonieczny <tsscamaro@gmail.com>
2017-05-16 10:34:39 -0400

    first commit

---
+++ HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using ADO_Practice.Models;
+
+
+namespace ADO_Practice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // connection string - need ip address, port number, database name, user name & password...
+            //SqlConnection c = new SqlConnection("Server=.\\sqlexpress;northwind;Trusted_Connection-True;");
+
+            //SqlCommand cmd = new SqlCommand();
+            //cmd.Connection = c;
+
+            //cmd.CommandText = "select count(*) from customers";
+            //***********to SEE HOW CUSTOMERS USING ENTITIY FRAMEWORK ******
+            //cmd.CommandType = System.Data.CommandType.Text;
+
+            //c.Open();
+
+            //int result = int.Parse(cmd.ExecuteScalar().ToString());
+
+            return View();
+        }
+
+    public ActionResult About()
+        {
+            NorthwindEntities NorthwindDB = new NorthwindEntities();
+
+            NorthwindDB.Customers.Count();
+
+            int count = NorthwindDB.Customers.Count();
+
+            ViewBag.Message = $"We have {count} Customers.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ NorthwindController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ADO_Practice.Models;
+using System.Data.SqlClient;
+
+namespace ADO_Practice.Controllers
+{
+    public class NorthwindController : Controller
+    {
+        // GET: Northwind
+        public ActionResult ListAllCustomers()
+        {
+            NorthwindEntities NE = new NorthwindEntities();
+
+            //select * from customers
+            List<Customer> CustomerList = NE.Customers.ToList();
+
+            ViewBag.Cities = GetAllCities();
+
+            ViewBag.CustomerList = CustomerList;
+
+            return View();
+        }
+        public ActionResult SearchCustomers(string CustomerID)
+        {
+            NorthwindEntities NE = new NorthwindEntities();
+
+            List<Customer> CustomerList = NE.Customers.Where(x => x.
+            CustomerID.ToUpper().
+            Contains(CustomerID.ToUpper())).ToList();
+
+            //foreach example.......
+            /* 
+             * List<Customer> CustomerList = new List<Customer>();
+                foreach (Customer element in NE.Customers)
+            {
+              if (element.CustomerID.ToUpper().Contains(CustomerID.ToUpper())).
+                       
+            }
+                CustomerList.Add(element);           
+             
+             */
+            ViewBag.Cities = GetAllCities();
+
+            ViewBag.CustomerList = CustomerList;
+
+            return View("ListAllCustomers");
+
+        }
+        public ActionResult SearchCustomersbyCity(string City)
+        {
+            NorthwindEntities NE = new NorthwindEntities();
+
+            List<Customer> CustomerList = NE.Customers.Where(x => x.
+            City.ToUpper().
+            Contains(City.ToUpper())).ToList();
+
+            ViewBag.Cities = GetAllCities();
+            ViewBag.CustomerList = CustomerList;
+
+            return View("ListAllCustomers");
+        }
+        public ActionResult DeleteCustomer(string CustomerID)
+        {
+            try
+            {
+                if (CustomerID == null)
+
+                {
+                    ViewBag.ErrorMessage("Customer ID incorect");
+                    return View("ErrorMessage");
+                }
+
+            NorthwindEntities NE = new NorthwindEntities();
+                //step #1 "find the custoemr that I need to delete....//
+                Customer ToDelete = NE.Customers.Find(CustomerID);
+                if(ToDelete == null)
+                {
+                    ViewBag.ErrorMessage = "Unavailable";
+                    return View("ErrorMessage");
+                }
+
+            //removes OBJECTS from DATABASE
+            
+            NE.Customers.Remove(ToDelete);
+
+            //to SAVE EVERYTHING TO DATABASE.....
+            NE.SaveChanges();
+
+            //REDIRECT....after DELETE...GO TO LIST ALL CUSTOMER ..
+            //EXECUTE the LISTALLCUSTOMER action
+            return RedirectToAction("ListAllCustomers", "Northwind");
+        }
+        catch (System.Data.Entity.Infrastructure.DbUpdateException)
+        {
+
+            ViewBag.ErrorMessage = "You Cannot delete a customer with ORDERS";
+            return View("ErrorMessage");
+        }
+          catch (Exception)
+            {
+                ViewBag.ErrorMessage = "OOOOPS....Something happened unexpected. Try Again!";
+                return View("ErrorMessage");
+            }
+        }
+        public ActionResult UpdateCustomer(string CustomerID)
+        {
+            //first thing...TO SHOW DATA
+
+            NorthwindEntities NE = new NorthwindEntities();
+
+            //update will SHOW up & THE MODEL will be updated......
+            Customer ToFind = NE.Customers.Find(CustomerID);
+
+            return View("CustomerDetails", ToFind);
+        }
+        public List<string> GetAllCities()
+        {
+            NorthwindEntities NE = new NorthwindEntities();
+
+
+            return NE.Customers.Select(x => x.City).Distinct().ToList();
+          
+
+        }
+        public ActionResult SaveUpdates(Customer ToBeUpdated)
+        {
+            NorthwindEntities NE = new NorthwindEntities();
+            //find the original customer record
+            Customer ToFind = NE.Customers.Find(ToBeUpdated.CustomerID);
+
+            ToFind.ContactName = ToBeUpdated.ContactName;
+
+            ToFind.Address = ToBeUpdated.Address;
+
+            ToFind.City = ToBeUpdated.City;
+
+            ToFind.ContactTitle = ToBeUpdated.ContactTitle;
+
+            ToFind.Phone = ToBeUpdated.Phone;
+
+            ToFind.Fax = ToBeUpdated.Fax;
+
+            ToFind.CompanyName = ToBeUpdated.CompanyName;
+
+            NE.SaveChanges();
+            return RedirectToAction("ListAllCustomers");
+
+        }
+        public ActionResult AddCustomer()
+        {
+            return View();
+            
+
+        }
+        public ActionResult SaveNewCustomer(Customer NewCustomer)
+        {
+            // TO DO VALIDATION!!!!! //
+            NorthwindEntities NE = new NorthwindEntities();
+
+            NE.Customers.Add(NewCustomer);
+
+            NE.SaveChanges();
+
+            return RedirectToAction("ListAllCustomers");
+
+        }
+    }
+}
